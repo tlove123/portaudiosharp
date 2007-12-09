453796acacabd627c44ad47ce5aa2470ade4e53b
R.Gerosa <R.Gerosa@8006bb54-9f19-0410-9ddd-133a698c9d03>
2007-12-09 16:36:24 +0000

    Changed version number to 0.19.2 Update copyright years in file headers

--- trunk/PortAudioSharp/src/AssemblyInfo.cs
+++ trunk/PortAudioSharp/src/AssemblyInfo.cs
@@ -1,6 +1,6 @@
  /*
   * PortAudioSharp - PortAudio bindings for .NET
-  * Copyright 2006, Riccardo Gerosa, and individual contributors as indicated
+  * Copyright 2006-2007, Riccardo Gerosa, and individual contributors as indicated
   * by the @authors tag. See the copyright.txt in the distribution for a
   * full listing of individual contributors.
   *
@@ -45,7 +45,7 @@ using System.Runtime.CompilerServices;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly: AssemblyVersion("0.19.1.*")]
+[assembly: AssemblyVersion("0.19.2.*")]
 
 // The following attributes specify the key for the sign of your assembly. See the
 // .NET Framework documentation for more information about signing.
